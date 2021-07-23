758f5b51610a457c342e75766aa29cc84a6800a9
Manoj-Kumar-Paliviri <83910643+Manoj-Kumar-Paliviri@users.noreply.github.com>
2021-07-23 23:00:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 st.title('Digit Classfier using Machine Learning')
 st.text('Upload the image of a digit to classify')
 model = joblib.load('Digit Classifier')
-uploaded_file = st.file_uploader("Choose an Image....",type = ['png','jepg','jpg'])
+uploaded_file = st.file_uploader("Choose an Image....",type = ['png','jpeg','jpg'])
 
 if uploaded_file is not None:
   img = Image.open(uploaded_file)
