6a1a6d51ab8df8aa6290b3f1976b408445fa7e6f
Manoj-Kumar-Paliviri <83910643+Manoj-Kumar-Paliviri@users.noreply.github.com>
2022-02-08 11:17:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,31 @@ import numpy as np
 from skimage.io import imread
 from skimage.transform import resize
 from PIL import Image
+import base64
+
+def set_bg_hack(main_bg):
+        '''
+        A function to unpack an image from root folder and set as bg.
+        The bg will be static and won't take resolution of device into account.
+        Returns
+        -------
+        The background.
+        '''
+        # set bg name
+        main_bg_ext = "png"
+            
+        st.markdown(
+            f"""
+            <style>
+            .stApp {{
+                background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+                background-size: cover
+            }}
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+set_bg_hack('background.png')
 st.title('Digit Classfier using Machine Learning')
 st.text('Upload the image of a digit to classify')
 model = joblib.load('Digit Classifier')
